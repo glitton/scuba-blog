83d9da0a0a6cb8f05b0d70ff9d0eb6aeb717d05c
glitton <glcodeworks@gmail.com>
2023-09-26 17:17:35 -0500

    changed vite config base to scuba-blog

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://glitton.github.io/scuba-blog/",
+  base: "/scuba-blog/",
   plugins: [react()],
 });
