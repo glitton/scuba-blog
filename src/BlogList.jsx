d3d568d0668fe91d5790218b2f3f629ab54c9761
glitton <glcodeworks@gmail.com>
2023-09-28 19:17:39 -0500

    graphQL endpoint 400 right now, wip

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,31 +1,63 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
-import { createClient } from "contentful";
+// import { createClient } from "contentful";
 import Bio from "./Bio";
 import { Link } from "react-router-dom";
 
+const blogQuery = `
+query {
+  blogCollection {
+    items {
+      blogTitle
+      createDate
+      blogSummary
+    }
+  }
+}
+`;
+
 const BlogList = () => {
   const [blogPosts, setBlogPosts] = useState([]);
-  const client = createClient({
-    space: import.meta.env.VITE_SPACE_ID,
-    accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
-  });
+  // const client = createClient({
+  //   space: import.meta.env.VITE_SPACE_ID,
+  //   accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+  // });
+  const spaceId = import.meta.env.VITE_SPACE_ID;
+  const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
 
   useEffect(() => {
-    const getAllBlogEntries = async () => {
-      try {
-        const entries = await client.getEntries({
-          content_type: "blog",
-          order: "-fields.createDate",
-        });
-        // console.log(typeof entries.items[0].fields.createDate);
-        setBlogPosts(entries);
-      } catch (error) {
-        console.log("error");
-      }
-    };
-    getAllBlogEntries();
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/${spaceId}/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ blogQuery }),
+      })
+      .then((response) => response.json())
+      .then((json) => console.log("data", json.data));
   }, []);
+
+  if (!blogPosts) {
+    return "Loading ...";
+  }
+
+  // useEffect(() => {
+  //   const getAllBlogEntries = async () => {
+  //     try {
+  //       const entries = await client.getEntries({
+  //         content_type: "blog",
+  //         order: "-fields.createDate",
+  //       });
+  //       // console.log(typeof entries.items[0].fields.createDate);
+  //       setBlogPosts(entries);
+  //     } catch (error) {
+  //       console.log("error");
+  //     }
+  //   };
+  //   getAllBlogEntries();
+  // }, []);
 
   return (
     <div className='global-wrapper'>
