93e5b340b5d30731648dd0e0d177574d922a15d7
glitton <glcodeworks@gmail.com>
2023-10-02 15:03:09 -0500

    added unpic, fixed bio avatar position

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -1,8 +1,9 @@
 import gscubaImage from "./images/gscuba.jpg";
+import { Image } from "@unpic/react";
 const Bio = () => {
   return (
     <div className='bio'>
-      <img src={gscubaImage} alt='gscuba' className='bio-avatar' />
+      <Image src={gscubaImage} alt='gscuba' className='bio-avatar' />
       <p>
         Posts and photography by <strong>Generosa Litton</strong>. I live in
         Puerto Morelos, Mexico and San Francisco, California. This site is
